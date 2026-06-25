78c6a71c7d2eb134715ea6e9ae22b53df80b502c
irikura <irikura.emerald@gmail.com>
2026-06-25 12:48:59 +0900

    DBにUserを追加

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,8 +2,7 @@ import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 import { faker } from "@faker-js/faker"
-import { TodoListCreateInput } from "@/app/generated/prisma/models";
-import { TodoCreateInput } from "@/app/generated/prisma/models";
+import { TodoCreateWithoutTodoListInput, TodoListCreateWithoutUserInput, UserCreateInput } from "@/app/generated/prisma/models";
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL,
@@ -13,15 +12,15 @@ const prisma = new PrismaClient({
     adapter,
 });
 
-function createTodos(): TodoCreateInput[] {
+function createTodos(): TodoCreateWithoutTodoListInput[] {
     const addDate = (object: Date, date: number) => object.setDate(object.getDate() + date);
     const from = addDate(new Date(), -7);
     const to = addDate(new Date(), 7);
 
     const NUMBER_OF_TODO = 10;
-    const todos: TodoCreateInput[] = [];
+    const todos: TodoCreateWithoutTodoListInput[] = [];
     for (let orderId = 1; orderId <= NUMBER_OF_TODO; orderId++) {
-        const todo: TodoCreateInput = {
+        const todo: TodoCreateWithoutTodoListInput = {
             name: faker.string.alphanumeric(100),
             detail: faker.string.alphanumeric(1000),
             deadline: faker.date.between({ from, to }),
@@ -32,11 +31,11 @@ function createTodos(): TodoCreateInput[] {
     return todos;
 }
 
-function createTodoLists(): TodoListCreateInput[] {
+function createTodoLists(): TodoListCreateWithoutUserInput[] {
     const NUMBER_OF_TODO_LIST = 10;
-    const todoLists: TodoListCreateInput[] = [];
+    const todoLists: TodoListCreateWithoutUserInput[] = [];
     for (let orderId = 1; orderId <= NUMBER_OF_TODO_LIST; orderId++) {
-        const todoList: TodoListCreateInput = {
+        const todoList: TodoListCreateWithoutUserInput = {
             name: faker.string.alphanumeric(100),
             orderId: orderId,
             todos: {
@@ -48,10 +47,27 @@ function createTodoLists(): TodoListCreateInput[] {
     return todoLists;
 }
 
+function createUsers(): UserCreateInput[] {
+    const NUMBER_OF_USER = 5;
+    const users: UserCreateInput[] = [];
+    for (let i = 0; i < NUMBER_OF_USER; i++) {
+        const user: UserCreateInput = {
+            name: faker.person.fullName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+            todoLists: {
+                create: createTodoLists(),
+            },
+        };
+        users.push(user);
+    }
+    return users;
+}
+
 export async function main() {
-    const todoLists = createTodoLists();
-    for (const todoList of todoLists) {
-        await prisma.todoList.create({ data: todoList });
+    const users = createUsers();
+    for (const user of users) {
+        await prisma.user.create({ data: user });
     }
 }
 
