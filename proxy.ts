56d1182120561b466e8dde7f473005cb10153c99
irikura <irikura.emerald@gmail.com>
2026-07-03 14:10:34 +0900

    未認証時にログインページへリダイレクトできるようにする

--- proxy.ts
+++ proxy.ts
@@ -1,1 +1,13 @@
-export { auth as proxy } from "@/auth";
+import { auth } from "@/auth"
+
+export const proxy = auth(request => {
+    // console.log(request.auth, request.nextUrl.pathname);
+    if (!request.auth && request.nextUrl.pathname !== "/signin") {
+        const newUrl = new URL("/signin", request.nextUrl.origin)
+        return Response.redirect(newUrl)
+    }
+});
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
