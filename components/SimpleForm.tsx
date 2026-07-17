e500ccb7dad387624c1175a86cd21775abada34e
irikura <irikura.emerald@gmail.com>
2026-07-17 18:54:06 +0900

    Changeイベントによる更新機能を実装

--- components/SimpleForm.tsx
+++ components/SimpleForm.tsx
@@ -1,6 +1,7 @@
 import yup from "@/yup.jp";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { TextField } from "@mui/material";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export type SimpleValidation = yup.ObjectSchema<{
@@ -20,13 +21,41 @@ type SimpleFormProps = {
     update: ({ id, value }: { id: number, value: string }) => void,
 };
 
+type FormValues = {
+    id: number,
+    value: string,
+};
+
 export default function SimpleForm({ label, type, id, value, validation, update }: SimpleFormProps) {
-    const { register, handleSubmit, setValues, getValues, formState: { errors } } = useForm<{ id: number, value: string }>({
+    const { register, handleSubmit, setValues, getValues, formState: { errors } } = useForm<FormValues>({
         resolver: yupResolver(validation),
     });
 
-    if (!getValues("value")) {
+    const formData = getValues();
+    if (!formData.id && !formData.value) {
         setValues({ id, value });
+    }
+
+    const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
+
+    function submit(formData: FormValues) {
+        clearTimeout(timeoutId);
+        console.log(timeoutId);
+        update(formData);
+        // console.log("onSubmit");
+    }
+
+    function change(formData: FormValues) {
+        const timeout = 3000;
+        const newTimeoutId = setTimeout(() => {
+            update(formData);
+            // console.log("onChange");
+        }, timeout);
+        setTimeoutId(oldTimeoutId => {
+            // console.log({ old: oldTimeoutId, new: newTimeoutId });
+            clearTimeout(oldTimeoutId);
+            return newTimeoutId;
+        });
     }
 
     const valueAttributes = {
@@ -38,7 +67,7 @@ export default function SimpleForm({ label, type, id, value, validation, update 
     }
 
     return (
-        <form onSubmit={handleSubmit(update)}>
+        <form onSubmit={handleSubmit(submit)} onChange={handleSubmit(change)}>
             <input type="hidden" {...register("id")} />
             <TextField margin="normal" {...valueAttributes} />
         </form>
