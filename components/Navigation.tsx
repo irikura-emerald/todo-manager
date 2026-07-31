592284229ab8d9c4ddf10bcb1a5c2acc34907bee
irikura <irikura.emerald@gmail.com>
2026-07-31 13:47:05 +0900

    ナビゲーションバーのデザイン調整

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,10 +3,16 @@ import { signOut } from "next-auth/react";
 
 export default function Navigation() {
     return (
-        <header>
-            <Link href="/">TODOリスト</Link>
-            <Link href="/user">利用者情報</Link>
-            <Button onClick={() => signOut()}>ログアウト</Button>
+        <header className="flex justify-between bg-blue-100">
+            <div className="flex items-center">
+                <div className="mx-3">
+                    <Link href="/" underline="hover">TODOリスト</Link>
+                </div>
+                <div className="mx-3">
+                    <Link href="/user" underline="hover">利用者情報</Link>
+                </div>
+            </div>
+            <Button onClick={() => signOut()} variant="text">ログアウト</Button>
         </header>
     );
 }
